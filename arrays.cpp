54db5bea0a43978d097aebd4ae576ce672bdd0f4
Jenit Lal Shakya <jenit9408@gmail.com>
2026-01-03 22:02:20 +0545

    Learned pointer in array

--- arrays.cpp
+++ arrays.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-    int a[] = {23, 75, 4, 5, 664, 45};
-    int size = sizeof(a) / sizeof(a[0]);    // this helps us to get the total length of the array
+    int marks[] = {23, 75, 49, 57, 64, 45};
+    int size = sizeof(marks) / sizeof(marks[0]);    // this helps us to get the total length of the array
 
     // using for loop
     cout<<"Array outpout using for loop:"<<endl;
     for (int i = 0; i < size; i++) {       // here value for 'i' only exist for 'for loop' only
-        cout<<"The value at index "<<i<<" is "<<a[i]<<endl;
+        cout<<"The value at index "<<i<<" is "<<marks[i]<<endl;
     }
     cout<<endl;
     
@@ -16,7 +16,7 @@ int main() {
     cout<<"Array outpout using while loop:"<<endl;
     int i = 0;
     while (i < size) {
-        cout<<"The value at index "<<i<<" is "<<a[i]<<endl;
+        cout<<"The value at index "<<i<<" is "<<marks[i]<<endl;
         i++;
     }
     cout<<endl;
@@ -25,10 +25,17 @@ int main() {
     int c = 0;
     cout<<"Array outpout using do while loop:"<<endl;
     do {
-        cout<<"The value at index "<<i<<" is "<<a[c]<<endl;
+        cout<<"The value at index "<<c<<" is "<<marks[c]<<endl;
         c++;
     } while (c < size);
     cout<<endl;
+
+    // Pointers in array
+    // array itself is a pointer so we don't need to use '&' while assigning the value
+    int* p = marks;
+    for (int i = 0; i < size; i++) {
+        cout<<"The value of marks["<<i<<"] is "<<*(p+i)<<endl;
+    }
     
     return 0;
 }
