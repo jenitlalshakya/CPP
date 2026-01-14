e87cf3ed15e561c4b4e73bdacec40677767a0473
Jenit Lal Shakya <jenit9408@gmail.com>
2026-01-14 22:32:28 +0545

    Learned inline function, default argument and recursion

--- recursion.cpp
+++ recursion.cpp
@@ -4,9 +4,9 @@ using namespace std;
 long long fact(int n) {
     if (n == 0 || n == 1) {
         return 1;
-    } else {
-        return n * fact(n - 1);
     }
+    
+    return n * fact(n - 1);
 }
 
 int main() {
